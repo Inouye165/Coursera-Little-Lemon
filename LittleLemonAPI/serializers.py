e7bd8de969bfbeba5797ec8f766e2f13d73efa93
inouye165 <inouye165@gmail.com>
2024-02-24 08:09:37 -0800

    Work done away from home

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from decimal import Decimal
 from .models import MenuItem
 from .models import Category
-
+from rest_framework.validators import UniqueValidator
 
 
         
@@ -22,10 +22,25 @@ class MenuItemSerializer(serializers.ModelSerializer):
     price_after_tax = serializers.SerializerMethodField(method_name = 'calculate_tax')
     category = CategorySerializer(read_only=True)
     category_id = serializers.IntegerField(write_only=True)
+    # price = serializers.DecimalField(max_digits=6, decimal_places=2, min_value=2)
+    # def validate_price(self, value):
+    #     if (value < 2):
+    #         raise serializers.ValidationError('Price should not be less than 2.0')
 
+    # def validate_stock(self, value):
+    #     if (value < 0):
+    #         raise serializers.ValidationError('Stock cannot be negative')
+    def validate(self, attrs):
+        if(attrs['price']<2):
+            raise serializers.ValidationError('Price should not be less than 2.0')
+        if(attrs['inventory']<0):
+            raise serializers.ValidationError('Stock cannot be negative')
+        return super().validate(attrs)
+    title = serializers.CharField(max_length=255,validators=[UniqueValidator(queryset=MenuItem.objects.all())])
     class Meta:
         model = MenuItem
         fields = ['id','title','price','stock', 'price_after_tax','category','category_id']
+        extra_kwargs = {'price': {'min_value': 2},'stock':{'source':'inventory', 'min_value': 0}}
         
     def calculate_tax(self,product:MenuItem):
         tax_calculated =  product.price * Decimal(1.1)
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,36 +1,50 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, renderer_classes
 from .models import MenuItem
 from .serializers import MenuItemSerializer
 from rest_framework import status
-from rest_framework import generics
+#from rest_framework import generics
 
-# class MenuItemsView(generics.ListCreateAPIView):
-#     queryset = MenuItem.objects.all()
-#     serializer_class = MenuItemSerializer
 
 from rest_framework.request import Request 
-class SingleItemMenuView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
+
+
+# class SingleItemMenuView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+#     queryset = MenuItem.objects.all()
+#     serializer_class = MenuItemSerializer
     
-    # The two methods below are added to make the single item view work with HyperlinkedRelatedField.  Instructions from Gemini
+#     # The two methods below are added to make the single item view work with HyperlinkedRelatedField.  Instructions from Gemini
 
-    def get_object(self):
-         obj = super().get_object()
-         return obj
+#     def get_object(self):
+#          obj = super().get_object()
+#          return obj
 
-    def retrieve(self, request: Request, *args, **kwargs):
-         instance = self.get_object()
-         serializer = self.get_serializer(instance, context={'request': request})
-         return Response(serializer.data)
+#     def retrieve(self, request: Request, *args, **kwargs):
+#          instance = self.get_object()
+#          serializer = self.get_serializer(instance, context={'request': request})
+#          return Response(serializer.data)
     
 @api_view(['GET','POST'])
 def MenuItems(request):
     if request.method == 'GET':
         items = MenuItem.objects.select_related('category').all()
+        category_name = request.query_params.get('category')
+        to_price = request.query_params.get('to_price')
+        search = request.query_params.get('search')
+        ordering = request.query_params.get('ordering')
+        if category_name:
+            items = items.filter(category__title__exact=category_name)
+        if to_price:
+            items = items.filter(price=to_price)
+        if search:
+            items = items.filter(title__startswith=search)
+        if ordering:
+            odering_fields = ordering.split(',')
+            items = items.order_by(*odering_fields)
         serialized_item = MenuItemSerializer(items, many=True)
+        
+
         return Response(serialized_item.data)
     if request.method == 'POST':
         serialized_item = MenuItemSerializer(data=request.data)
@@ -39,11 +53,6 @@ def MenuItems(request):
         return Response(serialized_item.data, status.HTTP_201_CREATED)
     
         
-# @api_view()
-# def single_item(request, id):
-#      item = get_object_or_404(MenuItem, pk=id)
-#      serializer = MenuItemSerializer(item, context={'request': request}) 
-#      return Response(serializer.data) 
 
 @api_view()
 def single_item(request, id):
@@ -59,27 +68,3 @@ def category_detail(request, pk):
     serialized_category = CategorySerializer(category)
     return Response(serialized_category.data)    
  
-# @api_view(['GET', 'POST'])
-# def menu_items(request):
-#     if request.method == 'GET':
-#         items = MenuItem.objects.all()
-#         serialized_item = MenuItemSerializer(items, many=True)
-#         return Response(serialized_item.data)  # Corrected to serialized_item.data
-    
-#     # elif request.method == 'POST':
-#     #     serialized_item = MenuItemSerializer(data=request.data)
-#     #     if serialized_item.is_valid(raise_exception=True):
-#     #         serialized_item.save()
-#     #         return Response(serialized_item.data, status=status.HTTP_201_CREATED)
-#     if request.method == 'POST':
-#         serialized_item = MenuItemSerializer(data=request.data)
-#         serialized_item.is_valid(raise_exception=True)
-#         serialized_item.save()
-#         return Response(serialized_item.data, status=status.HTTP_201_CREATED)
-
-# @api_view()
-# def single_item(request, id):
-#     item = get_object_or_404(MenuItem,pk=id)
-#     serialize_item = MenuItemSerializer(item)
-#     return Response(serialize_item.data)  
-
